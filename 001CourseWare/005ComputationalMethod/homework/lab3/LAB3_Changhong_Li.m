c81ca71d8c9da1070f9df0fb1475e4f1017eeb6e
CNStanLee <15284244183@163.com>
2023-09-28 18:35:28 +0100

    homework of cm and dsp

--- 001CourseWare/005ComputationalMethod/homework/lab3/LAB3_Changhong_Li.m
+++ 001CourseWare/005ComputationalMethod/homework/lab3/LAB3_Changhong_Li.m
@@ -1,78 +1,75 @@
-% Clean
+% *****Clean******%
 clear;
 close all;
 
+% ***** Demo Calculation time ******%
+tic;    % begin to record time
+A = rand(12000, 4400);  % generate random dataset A
+B = rand(12000, 4400);  % generate random dataset B
+disp(toc);  % display the time of generating the random dataset
+C = A .* B; 
+disp(toc);  % display the time of .*
 
-% Demo
-tic;
-A = rand(12000, 4400);
-B = rand(12000, 4400);
-disp(toc);
-
-C = A .* B;
-disp(toc);
-
-
-% Script
+% ***** Demo Calculation time of sum ******%
 sample_size = 5;
-% generate n by x times
 n = zeros(0, sample_size);
 time = zeros(0, sample_size);
+% generate n : n = 10^i
 for i = 1 : 1 : sample_size
     n(i) = 10 .^ i;
 end
-% begin to cal time
-tic;
-
-% cal the sum and record the running time
-
+tic;    % begin to cal time
+% call the function FindSum with dataset n & record time
 for i = 1 : 1 : sample_size
     FindSum(n(i));
     time(i) = toc; % save the cal time into the array
 end
-
-% plot and style
+% plot running time vs n
 figure;
 plot(n, time);
-title('Fig1 ExecTime to Num');
+title('Fig1 ExecTime to Num(SUM)');
 xlabel('Number n');
 ylabel('Execution Time');
 
-% compute the factorial of 5, 10, 15, 20
-for i = 5 : 5 : 20
-    disp(CalFactorial(i));
+% **** Compute the factorial of 5, 10, 15, 20 **%
+sample_size = 4;
+n = zeros(1, sample_size);
+time_of_fac = zeros(0, sample_size);
+tic;    % begin to record time 
+% call the function CalFactorial and record the running time
+for i = 1 : 1 : sample_size
+    disp(CalFactorial(i * 5));
+    n(i) = i * 5;
+    time_of_fac(i) = toc;
 end
+% plot running time(factorial) vs n
+% y = n!
+figure;
+plot(n, time_of_fac);
+title('Fig2 ExecTime to Num(factorial)');
+xlabel('Number i');
+ylabel('Execution Time');
 
-
-% Part 2 Re-design Lab2 Code
-%% 
-
-
-
+% *****Part 2 Re-design Lab2 Code******%
+% testify three function
 ErrorMAD(0.5);
 ErrorMAPE(0.5);
 ErrorRate(0.56);
-
-tic;
 % exhaustive approach
+tic;
 for i = 0.1 : 0.1 : 2
     step_size = 2-i;
     disp("step_size = " + step_size);
-
     current_err = ErrorRate(step_size);
-    disp("err=" + current_err);
- 
+    disp("err = " + current_err);
 end
-time1 = toc;
-
+time1 = toc;   % record time of exhaustive approach
 % adaptive h step method
 for i = 0.1 : 0.1 : 2
     step_size = 2-i;
     disp("step_size = " + step_size);
-
     current_err = ErrorRate(step_size);
     disp("err=" + current_err);
-
     if(current_err < 5)
       disp("The Critical Step Size has been found as " + step_size);
       disp("Error = " + current_err + "%");
@@ -80,14 +77,14 @@ for i = 0.1 : 0.1 : 2
       break;
     end  
 end
+% compare two excution time
 time2 = toc - time1;
 disp("exhaustive approach");
 disp(time1);
 disp("adaptive h");
 disp(time2);
 
-% Function Def
-
+% **************Function Def***************%
 % Find the sum of (1, n) and return
 function [sum] = FindSum( n )
     sum_l = 0 ;   % clean the sum local var
@@ -110,7 +107,6 @@ function [factorial] = CalFactorial(n)
         factorial = 1;
     end
 end
-
 
 % Computation of error between two solutions - mean abs difference
 function [mad_err] = ErrorMAD(h)
@@ -162,7 +158,6 @@ end
 
 % Computation of the error rate
 function [max_err] = ErrorRate(h)
-
     % Analy solution
     [va, ~] = AnalyticSolution(h);
     % Nume solution
@@ -170,7 +165,6 @@ function [max_err] = ErrorRate(h)
     % Find the mean abs diff
     n = length(va);
     err = zeros(1,n);
-
     for i = 1 : n
       if(va(i) == 0)
         err(i) = 0;
@@ -178,9 +172,7 @@ function [max_err] = ErrorRate(h)
         err(i) = (abs(va(i) - vn(i))./va(i)).*100;    %erro rate
       end
     end
-    
     max_err = max(err);
-
     disp('when h is');
     disp(h);
     disp('Max Error Rate is');
@@ -208,9 +200,7 @@ function [v, t] = NumericalSolution(h)
             v(i + 1) = v(i) + slope * h;
         end
 end
-%function_name : differential1
-%function_description : find dy/dt
-%slope = g - cvn/m
+% differential1
 function [out_slope] = differential1(input_y)
     %Const Def
     g = 9.81;
